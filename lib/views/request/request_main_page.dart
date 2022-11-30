b003b6883c39b72e0bf9028708ce55d612780558
Oscar CR <krz.oficial@gmail.com>
2022-11-30 12:49:03 -0600

    avance notificaciones en hilo

--- lib/views/request/request_main_page.dart
+++ lib/views/request/request_main_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:intranet_movil/model/request.dart';
 import 'package:intranet_movil/services/api_request.dart';
@@ -13,6 +16,7 @@ import 'package:intranet_movil/views/request/new_request.dart';
 import 'package:intranet_movil/widget/navigation_drawer_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 
 int id = 0;
 
@@ -73,6 +77,8 @@ class _HomeState extends State<RequestMainPage> {
     _requestPermissions();
     _createNotificationChannel();
     _repeatNotification();
+    initializeService();
+    onStart();
   }
 
   Stream<List<RequestModel>?> _request() async* {
@@ -314,6 +320,15 @@ class _HomeState extends State<RequestMainPage> {
   }
 
   Future<void> _repeatNotification() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    late List<RequestModel>? requestModel = [];
+
+    requestModel = (await ApiRequestService().getRequest(token.toString()))!
+        .cast<RequestModel>();
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails('Channel01', 'Solicitudes',
             channelDescription: 'Canal de notificaciones para las solicitudes',
@@ -325,9 +340,148 @@ class _HomeState extends State<RequestMainPage> {
     await flutterLocalNotificationsPlugin.periodicallyShow(
         id++,
         'Prueba',
-        'Notificacion en segundo planos',
+        requestModel!.length.toString(),
         RepeatInterval.everyMinute,
         notificationDetails,
         androidAllowWhileIdle: true);
   }
+
+  Future<void> onStart() async {
+    // Only available for flutter 3.0.0 and later
+    DartPluginRegistrant.ensureInitialized();
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    // bring to foreground
+    Timer.periodic(const Duration(seconds: 1), (timer) async {
+      _showNotification();
+    });
+  }
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  /// OPTIONAL, using custom notification channel id
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'my_foreground', // id
+    'MY FOREGROUND SERVICE', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.low, // importance must be at low or higher level
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+
+      notificationChannelId: 'my_foreground',
+      initialNotificationTitle: 'AWESOME SERVICE',
+      initialNotificationContent: 'Initializing',
+      foregroundServiceNotificationId: 888,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+
+      // this will be executed when app is in foreground in separated isolate
+      onForeground: onStart,
+
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+
+  service.startService();
+}
+
+// to ensure this is executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.reload();
+  final log = preferences.getStringList('log') ?? <String>[];
+  log.add(DateTime.now().toIso8601String());
+  await preferences.setStringList('log', log);
+
+  return true;
+}
+
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  // Only available for flutter 3.0.0 and later
+  DartPluginRegistrant.ensureInitialized();
+
+  // For flutter prior to version 3.0.0
+  // We have to register the plugin manually
+
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.setString("hello", "world");
+
+  /// OPTIONAL when use custom notification
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  // bring to foreground
+  Timer.periodic(const Duration(seconds: 10), (timer) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    late List<RequestModel>? requestModel = [];
+
+    requestModel = (await ApiRequestService().getRequest(token.toString()))!
+        .cast<RequestModel>();
+
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('Channel01', 'Solicitudes',
+            channelDescription: 'Canal de notificaciones para las solicitudes',
+            importance: Importance.max,
+            priority: Priority.high,
+            icon: "@mipmap/ic_launcher_round");
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+        id++,
+        'Prueba',
+        requestModel!.length.toString(),
+        RepeatInterval.everyMinute,
+        notificationDetails,
+        androidAllowWhileIdle: true);
+  });
+}
+
+Future<void> _showNotification() async {
+  const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails('Channel01', 'Solicitudes',
+          channelDescription: 'Canal de notificaciones para las solicitudes',
+          importance: Importance.max,
+          priority: Priority.high,
+          icon: "@mipmap/ic_launcher_round");
+  const NotificationDetails notificationDetails =
+      NotificationDetails(android: androidNotificationDetails);
+  await flutterLocalNotificationsPlugin.show(
+      id++, 'Intranet', 'Tu solicitud ha sido aprobada', notificationDetails);
 }
