c74cb728d52956035609e6000ab17507a3c02d83
Oscar-CR <krz.oficial@gmail.com>
2023-08-25 16:36:06 -0600

    agregado de listeners de eventos por dias

--- lib/views/room/room.dart
+++ lib/views/room/room.dart
@@ -3,7 +3,7 @@ import 'package:intranet_movil/model/manual.dart';
 import 'package:intranet_movil/utils/constants.dart';
 import 'package:intranet_movil/widget/navigation_drawer_widget.dart';
 import 'package:table_calendar/table_calendar.dart';
-
+import 'package:intl/intl.dart';
 
 class RoomPage extends StatefulWidget {
   const RoomPage({Key? key}) : super(key: key);
@@ -14,7 +14,11 @@ class RoomPage extends StatefulWidget {
 }
 
 class _RomState extends State<RoomPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
+  List<String> testDateList =  ['2023-08-21', ' 2023-08-21', '2023-08-21'];
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +28,52 @@ class _RomState extends State<RoomPage> {
         title: const Text(StringIntranetConstants.manualPage),
       ),
       body: TableCalendar(
-        firstDay: DateTime.utc(2010, 10, 16),
-        lastDay: DateTime.utc(2030, 3, 14),
-        focusedDay: DateTime.now(),
-      )
+        firstDay: kFirstDay,
+        lastDay: kLastDay,
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+
+            String formattedDate = DateFormat('yyyy-MM-dd').format(selectedDay);
+            if(testDateList.contains(formattedDate.toString())){
+              print('eventos disponibles de esta fecha');
+              print(selectedDay.toString());
+            }else{
+              print('eventos no disponible de esta fecha');
+              print(formattedDate.toString());
+            }
+          
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
+      ),
     );
   }
 }
+final kToday = DateTime.now();
+final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
